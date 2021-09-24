96f84f2447545da56b13852734df2a60117d69ed
taskattention <91316700+taskattention@users.noreply.github.com>
2021-09-24 15:13:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,15 +15,6 @@ if __name__ == '__main__':
    config.base_learner = Learner(config.image_size, config.bn_eps, config.bn_momentum, config.n_class,n_channels=config.n_channels,n_filters=config.n_filters).to(config.dev)
    config.meta_opt     = torch.optim.Adam
 
-
-   #-------    for tuning to add atention history if it is unavaible (during Ray )  ---------------#
-   #------- If you want to continue from 5000 iterations from ray to tune  comment this----------#
-   # if config.user == "TUNE_1" or "ANALYSIS":  
-   #    config.resume            = 0
-   #    model = algos[config.algo](config)  
-   #    model.save()  
-   #   #----------------------#   
-   # config.resume            = 0
    
    
    model                    = algos[config.algo](config) 
